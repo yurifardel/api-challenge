d19cfe317d347f667fc6fa0ccb65e7f94b85c27b
yuri fardel <yrnascimento@outlook.com>
2022-02-25 14:50:01 -0300

    make it better

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { books, characters }= require('./src/domain/usecases')
 
 const app = express()
 
-const PORT = process.env.PORT | 8080
+const PORT = process.env.PORT || 8080
 
 app.use(express.json())
 app.use(bodyParser.json())
