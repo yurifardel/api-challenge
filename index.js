a13d81b4e6913026dbe5c553848ba184127b1bc1
yuri fardel <yrnascimento@outlook.com>
2022-02-26 11:57:52 -0300

    ..

--- index.js
+++ index.js
@@ -1,30 +1,21 @@
-const express = require('express')
+const express = require("express");
 const bodyParser = require("body-parser");
-// const { books, characters, auth }= require('./src/domain/')
-const { DetailsCharacters } = require('./src/infra/model')
+const { books, characters, auth } = require("./src/domain/");
 
+const app = express();
 
-const app = express()
+const PORT = process.env.PORT || 8080;
 
-const PORT = process.env.PORT || 8080
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.json())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true} ))
+app.use(auth);
+app.use(characters);
+app.use(books);
 
-// app.use(auth)
-// app.use(characters)
-// app.use(books)
+app.get("/", (req, res) => {
+  res.send("hello world!!");
+});
 
-app.get('/', (req, res) => {
-  res.send('hello world!!')
-})
-
-app.get('/characters', async  (req, res, next) => {
-  const characters = await DetailsCharacters.find()
-
-  return res.json({ characters })
-})
-
-
-app.listen(PORT, () => console.log('express listening with stability'))
+app.listen(PORT, () => console.log("express listening with stability"));
