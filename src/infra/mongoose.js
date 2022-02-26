d11c9d63a5725da1d0113bdf8e61eba57e33b690
yuri fardel <yrnascimento@outlook.com>
2022-02-26 12:15:25 -0300

    refactor: url mongo

--- src/infra/mongoose.js
+++ src/infra/mongoose.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose')
-const path = require('path');
 require('dotenv').config()
 
 
-mongoose.connect('mongodb://0.0.0.0:27017/mobix', {
+mongoose.connect('mongodb://localhost:27017/mobix', {
   useNewUrlParser: true, 
   useUnifiedTopology: true
 })
--- src/infra/tests/mongo.spec.js
+++ src/infra/tests/mongo.spec.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 describe('Mongo helper', () => {
   beforeAll(async () => {
-    await mongoose.connect('mongodb://0.0.0.0:27017/mobix')
+    await mongoose.connect('mongodb://localhost:27017/mobix')
   })
   test('should connect mongodb', () => {
     expect(mongoose).toBeTruthy()
